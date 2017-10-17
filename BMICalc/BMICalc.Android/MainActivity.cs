f04028c3fce6e39deebd34f85cfcf90d53a266ae
aarone2 <aarone2@users.noreply.github.com>
2017-10-17 13:43:35 -0600

    Absolute Final Project

---
+++ BMICalc/BMICalc.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BMICalc.Android
+{
+    [Activity(Label = "BMICalc", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        Button bmiButton;
+        TextView bmiText;
+        EditText heightEditText, weightEditText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            bmiButton = FindViewById<Button> (BMICalc.Android.Resource.Id.bmiButton);
+            bmiText = FindViewById<TextView> (BMICalc.Android.Resource.Id.bmiText);
+            heightEditText = FindViewById<EditText> (BMICalc.Android.Resource.Id.heightEditText);
+            weightEditText = FindViewById<EditText>(BMICalc.Android.Resource.Id.weightEditText);
+
+            bmiButton.Click+= BmiButton_Click;
+        }
+
+        void BmiButton_Click(object sender, System.EventArgs e)
+        {
+            float height = float.Parse(heightEditText.Text);
+            float weight = float.Parse(weightEditText.Text);
+
+            float bmi = (weight / (height * height));
+
+            bmiText.Text = bmi.ToString();
+
+        }
+    }
+}
+
---
+++ BMICalc/BMICalc/ViewController.designer.cs
@@ -0,0 +1,55 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace BMICalc
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton bmiButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel bmiLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField heightField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField weightField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (bmiButton != null) {
+                bmiButton.Dispose ();
+                bmiButton = null;
+            }
+
+            if (bmiLabel != null) {
+                bmiLabel.Dispose ();
+                bmiLabel = null;
+            }
+
+            if (heightField != null) {
+                heightField.Dispose ();
+                heightField = null;
+            }
+
+            if (weightField != null) {
+                weightField.Dispose ();
+                weightField = null;
+            }
+        }
+    }
+}
